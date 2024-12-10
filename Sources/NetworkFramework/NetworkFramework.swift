c44908769697c8d0926f8ce6643f9ce67e461aa0
Mahmoud Alaa <143525272+MahmoudAlaa92@users.noreply.github.com>
2024-12-10 21:31:20 +0200

    make package public

--- Sources/NetworkFramework/NetworkFramework.swift
+++ Sources/NetworkFramework/NetworkFramework.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Alamofire
 
-struct User: Decodable {
+public struct User: Decodable, Sendable {
     let id: Int?
     let title: String?
     let price: Double?
@@ -9,7 +9,7 @@ struct User: Decodable {
 
 public class NetworkManager {
  
-    func fetchData(completion:@Sendable @escaping (Result< [User], AFError>) -> Void ) {
+    public static func fetchData(completion:@Sendable @escaping (Result< [User], AFError>) -> Void ) {
         
         let url = URL(string: "https://fakestoreapi.com/products")!
         
